9efcbffc4e8f46a9224d2ad460540a347a18b508
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-07-15 23:58:01 +0530

    Time: 3 ms (92.12%), Space: 9.2 MB (81.22%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
-    bool isCommonPrefix(vector<string>& strs, int len){
-        string str1 = strs[0].substr(0, len);
-        //check str1 is the prefix of all strs[1] to strs[n-1]
-        for(int i = 1; i < strs.size(); i++){
-            //str1.rfind(str2, 0) == 0: str1.startswith(str2)
-            if(strs[i].rfind(str1, 0) != 0) return false;
-        }
-        return true;
-    }
     string longestCommonPrefix(vector<string>& strs) {
         if(strs.size() == 0) return "";
-        int _min = INT_MAX;
-        for(string str : strs) _min = min(_min, (int)str.size());
-        //use binary search to find 
-        int low = 1, high = _min;
-        while(low <= high){
-            int middle = (low+high)/2;
-            if(isCommonPrefix(strs, middle)){
-                low = middle + 1;
-            }else{
-                high = middle - 1;
+        for(int i = 0; i < strs[0].size(); i++){
+            //i: index of char
+            for(int j = 1; j < strs.size(); j++){
+                //j: index of str
+                if(i >= strs[j].size() || strs[0][i] != strs[j][i]){
+                    return strs[0].substr(0, i);
+                }
             }
         }
-        //same result:
-        // return strs[0].substr(0, (low+high)/2);
-        return strs[0].substr(0, high);
+        return strs[0];
     }
 };
