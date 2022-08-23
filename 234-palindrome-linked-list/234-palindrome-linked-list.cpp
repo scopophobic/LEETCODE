3e427ba055ba15b1f7e083013783581092191b49
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-08-23 23:22:42 +0530

    Time: 370 ms (51.32%), Space: 114.2 MB (74.71%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,28 +1,45 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> v;
-        ListNode* cur = head;
-         while(cur){
-            v.push_back(cur->val);
-            cur = cur->next;
+        ListNode *slow = head, *fast = head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int n=v.size();
-        for(int i =0; i<n/2;i++){
-            if(v[i]!=v[n-i-1]){
-                return false;
-            }
+        
+        if(fast != NULL){
+            //the length of list is odd
+            //in this situation we should ignore the middle node
+            slow = slow->next;
         }
+        
+        //slow: later part of original list
+        //fast: former part of original list
+        slow = reverse(slow);
+        fast = head;
+        
+        while(slow != NULL){
+            if(slow->val != fast->val) return false;
+            slow = slow->next;
+            fast = fast->next;
+        }
+        
         return true;
+    }
+    
+    ListNode* reverse(ListNode* head){
+        //reverse the later part of original list
+        ListNode* prev = NULL;
+        ListNode* tmp;
+        while(head != NULL){
+            tmp = head->next;
+            head->next = prev;
+            prev = head;
+            head = tmp;
+        }
+        //when the loop finishes, head is NULL, 
+        //and prev is the last node in the original list
+        return prev;
     }
 };
