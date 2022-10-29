934a26f6dc829115ab48b6fa0cd2b1fd5e095fb8
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-10-30 04:32:24 +0530

    Time: 11 ms (23.33%), Space: 6 MB (24.80%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return pow(x,0.5);
+        if(x<=0) return x;
+        long long int low=1;
+        long long int hi=x-1;  //x=10
+        long long int mid;   //1 2 3 4 5 6 7 8 9 10
+        while(low<=hi){
+            mid=(hi+low)/2;
+            long long int j=mid*mid;
+            if(j==x) {
+                return mid;
+            }
+            
+            if(j>x){
+                hi=mid-1;
+            } 
+            else {
+                low=mid+1;
+            }
+        }
+        if(low*low<=x) return low;
+        else return hi;
     }
 };
