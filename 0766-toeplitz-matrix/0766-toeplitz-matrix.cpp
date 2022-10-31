af2d9e9b76b925ef3e4a2374c3abdeb83418ad4e
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-10-31 12:52:11 +0530

    Time: 27 ms (66.19%), Space: 17.3 MB (85.40%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         bool flag=true;
+        int row=matrix[0].size();
         int col=matrix.size();
-        for(int i=1;i<col;i++){
-            int row=matrix[i].size();
-            for(int j=1;j<row;j++){
-                if(matrix[i][j]!=matrix[i-1][j-1]){
+        for(int i=0;i<col-1;i++){
+          
+            for(int j=0;j<row-1;j++){
+                if(matrix[i][j]!=matrix[i+1][j+1]){
                     return false;
                     break;
                 }
