142e38e135fa478f0f40d0dfa983f70940446d96
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-12-05 05:28:06 +0530

    Time: 336 ms (30.37%), Space: 79.2 MB (52.93%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -36,57 +36,29 @@ public:
         
         
         
-//         int n=nums.size();
-//         if(n==1)    return 0;
-//         long long int pre[n];
-//         pre[0]=0;
+        int n=nums.size();
+        if(n==1)    return 0;
+        long long int pre[n];
+        pre[0]=nums[0];
         
-//         for(int i=1;i<n;i++) pre[i]=pre[i-1]+nums[i];
+        for(int i=1;i<n;i++) pre[i]=pre[i-1]+nums[i];
         
-//         int min=INT_MAX;
-//         int ind=0;
+        long long min=INT_MAX;
+        int ind=0;
         
-//         for(int i =1;i<n;i++){
-//             int k=(abs(pre[i-1]/i-(pre[n-1]-pre[i-1])/(n-i)));
-//             if(min>k){ 
-//                 min=k;
-//                 ind=i-1;
-//             }
-//         }
-//         if(min>abs(pre[n-1]/n)){
-//         min=abs(pre[n-1]/n);
-//         ind=n-1;
-//     }
-//         return ind;
-        
-        
-        
-    int n=nums.size();
-    if(n==1)    return 0;
-    long long pre[n]; //to store prefix sum
-    pre[0]=nums[0];
-	
-	//calculating prefix sum
-    for(int i=1;i<n;i++)    pre[i]=pre[i-1]+nums[i];
-	
-    long long res=INT_MAX;
-    int ind=0;
-	
-	// calculating minimum average for i=0 to i=n-2
-    for(int i=1;i<n;i++) {
-        int k=(abs(pre[i-1]/i-(pre[n-1]-pre[i-1])/(n-i)));
-        if(res>k){
-            res=k;
-            ind=i-1;
+        for(int i =1;i<n;i++){
+            int k=(abs(pre[i-1]/i-(pre[n-1]-pre[i-1])/(n-i)));
+            if(min>k){ 
+                min=k;
+                ind=i-1;
+            }
         }
-    }
-	
-	//special case for i=n-1
-    if(res>abs(pre[n-1]/n)){
-        res=abs(pre[n-1]/n);
+        if(min>abs(pre[n-1]/n)){
+        min=abs(pre[n-1]/n);
         ind=n-1;
     }
-    return ind;
+        return ind;
+        
     
     }
 };
