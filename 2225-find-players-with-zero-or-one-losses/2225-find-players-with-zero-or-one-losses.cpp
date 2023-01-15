9cc36e3170741127e77f8a0bd123772f8bfb1923
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-01-15 14:55:36 +0530

    Time: 1803 ms (8.84%), Space: 153.2 MB (95.77%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        map<int,int>win,lose;
-        unordered_set<int> s;
-        for(int i=0;i<matches.size();i++){
-            win[matches[i][0]]++;
-            lose[matches[i][1]]++;
-            s.insert(matches[i][0]);
-            s.insert(matches[i][1]);
-        }
-        vector<vector<int>>ans;
-        vector<int> temp1;
-        vector<int> temp2;
-        for(auto it : s){
-            
-            if(win[it]>0 and lose[it]==0) temp1.push_back(it);
-            if( lose[it]==1) temp2.push_back(it);
-            
-        }
-        sort(temp1.begin(),temp1.end());
-        sort(temp2.begin(),temp2.end());
-        ans.push_back(temp1);
-        ans.push_back(temp2);
-        
-        return ans;
-        
+ 
+unordered_map<int,int>mp;
+ 
+vector<vector<int>>ans;
+ 
+ 
+     for(int i = 0;i<matches.size(); i++){
+        mp[matches[i][1]]++;
+     }
+ 
+      vector<int>temp1;
+      vector<int>temp2;
+ 
+     for(int i = 0; i<matches.size(); i++){
+         
+         if(mp.find(matches[i][0]) == mp.end() && find(temp1.begin(),temp1.end(),matches[i][0]) == temp1.end() ){
+              temp1.push_back(matches[i][0]);
+         }
+     }
+ 
+     for(auto it : mp){
+         if(it.second == 1){
+             temp2.push_back(it.first);
+         }
+     }
+ 
+     sort(temp1.begin(),temp1.end());
+     sort(temp2.begin(),temp2.end());
+     ans.push_back(temp1);
+     ans.push_back(temp2);
+     return ans;  
     }
 };
