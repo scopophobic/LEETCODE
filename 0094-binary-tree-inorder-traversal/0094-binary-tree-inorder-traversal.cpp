e9e981757f38df4ad8b446e049758066aa8cdfbb
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-01-24 05:10:56 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (17.27%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,23 +1,51 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
 class Solution {
 public:
-    vector<int>ans;
+    
     vector<int> inorderTraversal(TreeNode* root) {
-        if(root==NULL) return {};
-        
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
+        vector<int>ans;
+        stack<TreeNode*>s;
+        TreeNode *curr=root;
+        while(!s.empty() || curr){
+            if(curr){
+                s.push(curr);
+                curr=curr->left;
+            }else {
+                curr=s.top();
+                s.pop();
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+        }
         return ans;
+        
     }
-};
+
+// vector<int> inorderTraversal(TreeNode* root) {
+//     vector<int> ans;
+//     stack<TreeNode*> s;
+//     TreeNode* cur = root;
+//     while (!s.empty() || cur) {
+//         if (cur) {
+//             s.push(cur);
+//             cur = cur->left;
+//         } else {
+//             cur = s.top();
+//             s.pop();
+//             ans.push_back(cur->val);
+//             cur = cur->right;
+//         }
+//     }
+//     return ans;
+// }
+    };
