7cf7afc9465228801a04bd77bf130c76387328c6
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-03-12 15:12:10 +0530

    Time: 44 ms (56.70%), Space: 89.2 MB (92.00%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-       vector<int> ans;
-       if(nums1.size()) for(auto it : nums1) ans.push_back(it);
-       if(nums2.size()) for(auto it : nums2) ans.push_back(it);
-       sort(ans.begin(), ans.end());
-       
-       int n = ans.size();
-       if(n % 2 == 0){
-           double a = (double)(ans[n/2 - 1] + ans[n/2])/2;
-           return a;
-       } else {
-           return (double)ans[n/2];
-       }
+        int n=nums1.size();
+        int m=nums2.size();
+        if (n == 0 && m == 0) {
+            return 0.0;
+        }
+        if (n > m) {
+            swap(nums1, nums2);
+            swap(n, m);
+        }
+        int i=0,j=0;
+        int left = 0, right = n;
+        while (left <= right) {
+            i = (left + right) / 2;
+            j = (n + m + 1) / 2 - i;
+            if (i < n && nums2[j-1] > nums1[i]) {
+                left = i + 1;
+            }
+            else if (i > 0 && nums1[i-1] > nums2[j]) {
+                right = i - 1;
+            }
+            else {
+                int max_left = 0;
+                if (i == 0) max_left = nums2[j-1];
+                else if (j == 0) max_left = nums1[i-1];
+                else max_left = max(nums1[i-1], nums2[j-1]);
+                if ((n + m) % 2 == 1) {
+                    return max_left;
+                }
+                int min_right = 0;
+                if (i == n) min_right = nums2[j];
+                else if (j == m) min_right = nums1[i];
+                else min_right = min(nums1[i], nums2[j]);
+                return (max_left + min_right) / 2.0;
+            }
+        }
+        return 0.0;
     }
 };
