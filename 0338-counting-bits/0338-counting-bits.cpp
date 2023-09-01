2ce4c8a437ba8a0603f8e3b0014eec3fb7654b1b
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-09-01 14:17:04 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (42.13%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -3,18 +3,14 @@ public:
     vector<int> countBits(int n) {
         vector<int>ans;
         for(int i=0;i<=n;i++){
-            // int val=i;
-            // int temp=0;
-            // while(val>0){
-            //     if(val%2==0) {
-            //         temp+=1;
-            //         break;
-            //     }
-            //     temp+=i%2;
-            //     // if(val==2) temp++;
-            //     val/=2;
-            // }
-            ans.push_back(__builtin_popcount(i));
+            int val=i;
+            int temp=0;
+            while(val!=0){
+                temp+=val%2;
+                // if(val==2) temp++;
+                val/=2;
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
