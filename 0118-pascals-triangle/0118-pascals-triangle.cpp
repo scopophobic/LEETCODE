9170dd8c1a4364ae11b6c627b2f759fa50887f5e
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-09-08 07:48:38 +0530

    Time: 4 ms (27.26%), Space: 6.8 MB (20.86%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans;
+        vector<vector<int>>pas;
         if(numRows == 1) return {{1}};
-        ans.push_back({1});
-        ans.push_back({1,1});
-        int n=1;
-        for(int i=0;i<numRows-2;i++){
+        pas.push_back({1});
+        pas.push_back({1,1});
+        
+        for(int i=2;i<numRows;i++){
             vector<int>temp;
-            int o=ans[n].size();
-            // int a=0;
             temp.push_back(1);
-            for(int j=0;j<o-1;j++){
-                
-                int a=ans[n][j]+ans[n][j+1];
-                temp.push_back(a);
-
+            for(int j=0;j<i-1;j++){
+                temp.push_back(pas[i-1][j]+pas[i-1][j+1]);
             }
             temp.push_back(1);
-            ans.push_back(temp);
-            n++;
+            pas.push_back(temp);
         }
-        return ans;
+        return pas;
+        
     }
 };
