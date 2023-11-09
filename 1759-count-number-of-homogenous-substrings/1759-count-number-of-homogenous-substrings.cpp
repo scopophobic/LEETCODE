00bc3a44716d54e76f6d314735b3ad024c5f6023
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-11-10 02:19:57 +0530

    Time: 115 ms (5.09%), Space: 11.9 MB (87.83%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     const unsigned int M = 1000000007;
-    unsigned long long int fact(unsigned int n) { 
+    long long int fact(unsigned int n) { 
         if (n == 1 ) 
             return 1; 
         return n + fact(n - 1); 
     } 
     int countHomogenous(string s) {
         char x=s[0];
-        unsigned long long int ans=0,c=1;
+        long long int ans=0,c=1;
         
         
         for(int i=1;i<s.size();i++){
             if(s[i]!=x){
-                ans+=fact(c);
                 cout<<c<<" "<<ans<<endl;
+                x=s[i];
                 c=0;
             }
             c++;
-            x=s[i];
+            ans+=c;
+            
         }
-        ans+=fact(c);
+        // ans+=fact(c);
         
-        return ans%M;
+        return (ans+1)%M;
         
     }
 };
