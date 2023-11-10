6a45de947d2b13f414a3bdead35eacbe0b662ad2
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-11-10 17:27:40 +0530

    Time: 510 ms (31.21%), Space: 115.6 MB (70.91%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -18,13 +18,14 @@ public:
         
         // cout<<s;
         set<int>a;
-        int l=0;
         a.insert(s);
         vector<int>ans;
+        int left = -1000000;
+        int i=0,l=0;
         while(c--){
             // cout<<s<<" ";
-            ans.push_back(s);
-            cout<<s<<" ";
+            
+            // cout<<s<<" ";
             // for(int i=0;i<mp[s].size();i++){
             //     if (a.find(mp[s][i]) == a.end()){
             //         a.insert(mp[s][i]);
@@ -37,16 +38,24 @@ public:
             // ans.push_back(s);
             
             
-            if (a.find(mp[s][0]) == a.end()) {
-                a.insert(mp[s][0]);
-                s=mp[s][0];
-            }
-            else if (mp[s].size()==2 and a.find(mp[s][1]) == a.end()) {
-                a.insert(mp[s][1]);
-                s=mp[s][1];
-            }
+            // if (a.find(mp[s][0]) == a.end()) {
+            //     a.insert(mp[s][0]);
+            //     s=mp[s][0];
+            // }
+            // else if (mp[s].size()==2 and a.find(mp[s][1]) == a.end()) {
+            //     a.insert(mp[s][1]);
+            //     s=mp[s][1];
+            // }
+            if(mp[s].size()==1)l++;
+            int newval = (l<2 and mp[s][0] == left) ? mp[s][1] : mp[s][0];
+            // ans.push_back(newval);
+            ans.push_back(s);
+            left=s;
+            s=newval;
+            
 
-            if(mp[s].size()==1) l++;
+
+
             
            
         }
