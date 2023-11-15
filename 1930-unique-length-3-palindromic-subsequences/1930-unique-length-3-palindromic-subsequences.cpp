eba4f751fd37689e925cde1670d92b92c05afad4
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-11-16 03:13:48 +0530

    Time: 259 ms (58.25%), Space: 13.2 MB (80.49%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-//         map<char,pair<int, int>>mp;
-//         for(int i=0; i<26; i++){
-//             mp['a'+i].first=-1;
-//             mp['a'+i].second=-1;}
-//         for(int i=0;i<s.size() ;i++ ){
-//             if(mp[s[i]].first==-1) mp[s[i]].first=i;}
-//         for(int i=s.size()-1;i>=0 ;i--){
-//             if(mp[s[i]].second==-1) mp[s[i]].second=i;}
-        
-        int ans = 0;
-        set<char> uni;
+        int res = 0;
+        unordered_set<char> uniq;
 
         for (char c : s) {
-            uni.insert(c);
+            uniq.insert(c);
         }
-        
-        
 
-        for(char c : uni){
-            int a = s.find(c);
-            int b = s.rfind(c);
- 
-            // int a=it.second.first,b=it.second.second;
-            set<char>se;
-            if(a<b){
-            for(int i=a+1;i<b;i++) se.insert(s[i]);
-            ans+=se.size();
+        for (char c : uniq) {
+            int start = s.find(c);
+            int end = s.rfind(c);
+
+            if (start < end) {
+                unordered_set<char> charSet;
+                for (int i = start + 1; i < end; i++) {
+                    charSet.insert(s[i]);
+                }
+                res += charSet.size();
             }
         }
-        return ans;
+
+        return res;        
     }
 };
