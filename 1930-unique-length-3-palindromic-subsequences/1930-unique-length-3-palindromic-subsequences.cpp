0dc3f28d17807d9e816f39e8ee379bcc003cf4c6
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-11-16 02:53:21 +0530

    Time: 427 ms (23.45%), Space: 14 MB (23.86%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int res = 0;
-        unordered_set<char> uniq;
-
-        for (char c : s) {
-            uniq.insert(c);
-        }
-
-        for (char c : uniq) {
-            int start = s.find(c);
-            int end = s.rfind(c);
-
-            if (start < end) {
-                unordered_set<char> charSet;
-                for (int i = start + 1; i < end; i++) {
-                    charSet.insert(s[i]);
+        map<char,pair<int, int>>mp;
+        for(int i=0; i<26; i++){
+            mp['a'+i].first=-1;
+            mp['a'+i].second=-1;}
+        for(int i=0;i<s.size() ;i++ ){
+            if(mp[s[i]].first==-1) mp[s[i]].first=i;}
+        for(int i=s.size()-1;i>=0 ;i--){
+            if(mp[s[i]].second==-1) mp[s[i]].second=i;}
+        
+        
+        int ans=0;
+        for(auto it: mp){
+            if(it.second.first==-1) continue;
+            cout<<it.first<<" : ";
+            cout<<it.second.first<<" "<<it.second.second<<endl;
+            int a=it.second.first,b=it.second.second;
+            set<char>se;
+            for(int i=a+1;i<=b-1;i++){
+                auto pos = se.find(s[i]);
+                if(pos == se.end()){
+                    se.insert(s[i]);
+                    ans++;
                 }
-                res += charSet.size();
             }
         }
-
-        return res;        
+        
+        return ans;
     }
 };
