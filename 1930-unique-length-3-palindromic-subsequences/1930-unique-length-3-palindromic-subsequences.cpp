63339085c6ca5bfc20fcc2f7058fa5bb5bbfabb1
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-11-16 03:37:51 +0530

    Time: 641 ms (17.50%), Space: 13.9 MB (26.29%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-//         map<char,pair<int, int>>mp;
-//         for(int i=0; i<26; i++){
-//             mp['a'+i].first=-1;
-//             mp['a'+i].second=-1;}
-//         for(int i=0;i<s.size() ;i++ ){
-//             if(mp[s[i]].first==-1) mp[s[i]].first=i;}
-//         for(int i=s.size()-1;i>=0 ;i--){
-//             if(mp[s[i]].second==-1) mp[s[i]].second=i;}
-        
-        int ans = 0;
-        unordered_set<char> uni;
-
-        for (char c : s) {
-            uni.insert(c);
-        }
+        map<char,pair<int, int>>mp;
+        for(int i=0; i<26; i++){
+            mp['a'+i].first=-1;
+            mp['a'+i].second=-1;}
+        for(int i=0;i<s.size() ;i++ ){
+            if(mp[s[i]].first==-1) mp[s[i]].first=i;}
+        for(int i=s.size()-1;i>=0 ;i--){
+            if(mp[s[i]].second==-1) mp[s[i]].second=i;}
         
         
-
-        for(char c : uni){
-            int a = s.find(c);
-            int b = s.rfind(c);
+        int ans=0;
+        for(auto it: mp){
+            if(it.second.first==-1) continue;
  
-            // int a=it.second.first,b=it.second.second;
-             unordered_set<char>se;
-            if(a<b){
-            for(int i=a+1;i<b;i++) se.insert(s[i]);
-            ans+=se.size();
-            }
+            int a=it.second.first,b=it.second.second;
+            set<char>se;
+            for(int i=a+1;i<=b-1;i++){
+
+
+                    se.insert(s[i]);
+
+                }
+                
+            
+        ans+=se.size();
         }
+        
         return ans;
     }
 };
