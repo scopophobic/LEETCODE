de42de3d8c437f1416dcc9b39753ba4c42c83868
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2023-11-18 13:31:27 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (26.69%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    long long int binary(string s){
+        int j = 0;
+        long long int ans = 0;
+        for(int i = s.size() - 1; i >= 0; i--){
+            if(s[i] == '1'){
+                ans += pow(2, j);
+            }
+            j++;
+        }
+        return ans;
+    }
+
+    string intToBinary(int n,int no) {
+        if (n == 0) return "0";
+
+        const int numBits = no; // For example, let's assume 5 bits for illustration
+        string s(numBits, '0'); // Initialize the string with '0's
+
+        int i = numBits - 1; // Start from the end of the string
+
+        while (n > 0) {
+            s[i--] = (n % 2) + '0'; // Convert remainder to character
+            n /= 2;
+        }
+
+        return s;
+    }
+
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = pow(2, nums.size());
+        set<int> s;
+        for(int i = 0; i < nums.size(); i++){
+            long long int bin = binary(nums[i]);
+            s.insert(bin);
+        }
+        string ans(nums.size(),'0');
+        for (int i = 0; i < n; i++) {
+            if (s.find(i) == s.end()) {
+                if(i==0) return ans;
+                return intToBinary(i,nums.size());
+            }
+        }
+        return "";
+    }
+};
