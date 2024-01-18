5b322227d58fdce44d0e82ba10148a3e94199f48
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-01-18 15:25:29 +0530

    Time: 2 ms (45.07%), Space: 6.3 MB (88.87%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+       int arr[n+1];
+        arr[0]=1;
+        arr[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2];
+            
+            if(i==n){
+                return arr[i];
+            }
+        }
+        return 1;
+        
+        
+        
+    }
+};
