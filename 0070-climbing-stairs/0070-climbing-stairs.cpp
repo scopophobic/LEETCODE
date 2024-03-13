9a1b214d78f30b5f9d571545052f3075bfb4400f
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-03-14 04:08:50 +0530

    Time: 20 ms (17.53%), Space: 205.5 MB (11.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
+    
+    
     int climbStairs(int n) {
-       int arr[n+1];
-        arr[0]=1;
-        arr[1]=1;
+        vector<int>dp(1000100);
+
         
-        for(int i=2;i<=n;i++){
-            arr[i]=arr[i-1]+arr[i-2];
-            
-            if(i==n){
-                return arr[i];
+        for(int i=0;i<=n;i++){
+            if(i==0){
+                dp[i]=1;
+            }
+            else{
+                dp[i]=0;
+                for(int j=1;j<=2;j++){
+                    if(i-j >= 0){
+                        dp[i] += dp[i-j];
+                    }
+                }
             }
         }
-        return 1;
         
         
-        
+        // for(auto it : ans) cout<<it<<" ";
+        return dp[n];
     }
 };
