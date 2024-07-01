eff1321d5aa7ead077bbfdadb6eda2d7dd2af122
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-07-02 03:21:16 +0530

    Time: 63 ms (29.63%), Space: 51.2 MB (92.59%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.ts
@@ -0,0 +1,17 @@
+function threeConsecutiveOdds(arr: number[]): boolean {
+    let count = 0; 
+    let n = arr.length;
+    for(let i=0;i<n;i++){
+        if(arr[i]%2!=0){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        
+        
+        if(count == 3 ) return true;
+    }
+    
+    return false;
+};
