6b0ba260f9e4555d134e339cfee5acfac5daa0e8
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-07-21 06:58:07 +0530

    Time: 14 ms (47.25%), Space: 22.8 MB (27.55%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -13,31 +13,21 @@ class Solution {
 public:
    void addVec(TreeNode* root,int &k,int &ans){
        if(root== nullptr ) return;
-      
-
+     
         
         addVec(root->left,k,ans);
-      
          if (--k == 0) {
             ans = root->val;
             return;  
         }
         addVec(root->right,k,ans);
         
-       
-
+    
    }
     
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>temp;
         int ans = -1;
-        // temp.push_back(root->val);
         addVec(root,k,ans);
-        
-        
-        for(auto it : temp) cout<<it<<" ";
-        cout<<"\n";
-
         return ans;
     }
 };
