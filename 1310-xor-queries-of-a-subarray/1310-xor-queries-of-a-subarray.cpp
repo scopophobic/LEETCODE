72587167638cca7f415ec48eb3cd522e5a477cfc
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-09-13 21:42:42 +0530

    Time: 1188 ms (13.93%), Space: 44.6 MB (24.27%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -4,16 +4,20 @@ public:
         vector<int>ans;
         for(auto range:queries){
             int start=range[0],end=range[1];
-            
-
+            if(start==end){
+                ans.push_back(arr[start]);
+                continue;
+            }
+            int x_or=arr[start]^arr[start+1];
+            // int x_or = 0;
             int temp=0;
-            for(int i=start;i<=end;i++){
-
-                temp^=arr[i];
+            for(int i=start+2;i<=end;i++){
+                temp=x_or;
+                x_or^=arr[i];
                 // cout<<x_or<<" ";
             }
             cout<<endl;
-            ans.push_back(temp);
+            ans.push_back(x_or);
         }
         return ans;
     }
