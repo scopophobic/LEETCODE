8a17510b4a8d9877420e78085a7fc6128fc8c116
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-09-19 08:03:13 +0530

    Time: 13 ms (10.84%), Space: 15.5 MB (85.37%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,28 @@
+
+bool compare (int a, int b){
+        string x = to_string(a);
+        string y = to_string(b);
+        
+        if((x+y)>(y+x)) return true;
+        else return false;
+        
+    }
+class Solution {
+public:
+    
+    string largestNumber(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end(),compare);
+        
+        if (nums[0] == 0) {
+            return "0";
+        }
+        
+        string ans= "";
+        for(auto it : nums){
+            ans+=to_string(it);
+        }
+        
+        return ans;
+    }
+};
