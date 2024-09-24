d6a96b6397be185ba295fa0fa9f150f0d866c54c
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-09-24 13:16:25 +0530

    Time: 861 ms (5.11%), Space: 294.9 MB (5.10%) - LeetHub

---
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -0,0 +1,27 @@
+vector<int>dp(1000,-1);
+class Solution {
+
+
+public:
+    int dfs (int i, int n,string s,set<string>words){
+        if(i==n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        
+        int res = 1 + dfs(i+1,n,s,words);
+        
+        for(int j=i;j<n;j++){
+            string temp = s.substr(i, j - i + 1);
+            if(words.find(temp)!=words.end())
+                res = min(res, dfs(j + 1, n, s, words));
+        }
+        dp[i] = res;
+        
+        return res;
+        
+    }
+    int minExtraChar(string s, vector<string>& dictionary) {
+        set<string> words(dictionary.begin(), dictionary.end()); 
+        fill(dp.begin(), dp.end(), -1);
+        return dfs(0,s.size(),s,words);
+    }
+};
