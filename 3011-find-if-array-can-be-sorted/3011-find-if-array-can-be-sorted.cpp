2ae8b666d7f43f432963feca4c2384382ac4257a
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-11-06 11:37:49 +0530

    Time: 39 ms (7.19%), Space: 34.4 MB (7.98%) - LeetHub

---
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    unsigned int setBits(int n)
+    {
+        unsigned int count = 0;
+        while (n) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+    
+    bool canSortArray(vector<int>& nums) {
+        
+        int n = nums.size();
+        map<int,int>cbits;
+        vector<int>temp = nums;
+        sort(temp.begin(),temp.end());
+        if(temp == nums) return true;
+        
+        for(int i=0;i<n;i++){
+            cbits[nums[i]] = setBits(nums[i]);
+        }
+        // for(int i=0;i<n;i++){
+        //     if()
+        // }
+        for(int j=0;j<n;j++){
+        for(int i=0;i<n-1;i++){
+            // for(int j=i;j<n;j++){
+            //     if(nums[i] > nums[j] and cbits[nums[j]] == cbits[nums[i]]){
+            //         cout<<nums[i]<<" "<<nums[j]<<endl;
+            //         swap(nums[i],nums[j]);
+            //     }
+            // }
+            if(nums[i] > nums[i+1] and cbits[nums[i+1]] == cbits[nums[i]]){
+                    // cout<<nums[i]<<" "<<nums[j]<<endl;
+                    swap(nums[i],nums[i+1]);
+            }
+        }
+        }
+        // cout<<"HI"<<endl;
+        // for(auto it : nums) cout<<it<<" ";
+        // cout<<endl;
+        // for(auto it : cbits) cout<<it.first<<" : "<<it.second<<endl;
+        // cout<<endl;
+        if(temp == nums) return true;
+        
+        return false;
+        
+    }
+};
