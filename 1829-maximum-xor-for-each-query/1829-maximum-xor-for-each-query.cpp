a9ed8b137cc8004de036dba8fcb56dfb95d535dc
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2024-11-08 21:57:19 +0530

    Time: 0 ms (100.00%), Space: 95.3 MB (92.93%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -4,20 +4,16 @@ public:
         // return {2^3^4^7^9};
         int k = pow(2,bit) - 1;
         vector<int>arr(nums.size());
-        int sum=0;
+        int sum=0,j=nums.size()-1;
         for(int i=0;i<nums.size();i++){
             sum ^= nums[i];
-            arr[i] = sum;
+            arr[j] = k - sum;
+            j--;
         }
         int i=0;
-        for(auto it : arr){
-            cout<<it<<" ";
-            nums[i] = k - it ;
-            i++;
-        } 
         
-        cout<<endl;
-        reverse(nums.begin(),nums.end());
-        return nums;
+
+        // reverse(arr.begin(),arr.end());
+        return arr;
     }
 };
